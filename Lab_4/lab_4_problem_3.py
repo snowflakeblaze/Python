706098f0a19b3d6a3705e9660ecc213c2ae90145
snowflakeblaze <snowflakeblazeinc@gmail.com>
2022-09-27 11:05:35 -0500

    completed problem 3

--- Lab_4/lab_4_problem_3.py
+++ Lab_4/lab_4_problem_3.py
@@ -2,3 +2,16 @@
 ## Design a program that keeps a running total of the number of bugs collected during seven days. 
 ## The loop should ask for the number of bugs collected for each day, and 
 ## when the loop is finished, the program should display the total number of bugs collected.
+
+print ("\nWelcome\n\n")
+
+print ("This program is going to calculate the amount of bugs you collected during the past 7 days\n\n")
+
+bugs_collected = 0
+
+for x in range(7):
+    daily_input = int(input("Please enter how many bugs you collected on day " + str(x + 1) + ": \n"))
+    bugs_collected = bugs_collected + daily_input
+
+print ("\nTotal bugs colected: " + str(bugs_collected) + "\n\n")
+print ("Have a Great Day\n")
